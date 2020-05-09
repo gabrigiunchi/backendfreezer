83d6f33ffe2de9f7d30d36c37e7bb98ff89ba19d
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2020-05-09 12:47:03 +0200

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-# gcloud auth activate-service-account --key-file service-account-key.json
-# gcloud config set project freezer-276212
-gcloud activa-service-account --key-file service-account-key.json
+gcloud auth activate-service-account --key-file service-account-key.json
 mvn clean
 mvn package -DskipTests -e
 mvn appengine:deploy
